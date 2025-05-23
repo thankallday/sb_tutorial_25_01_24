901f9b6f2a5959623cc0f1772205610d230363f9
thankallday <thankallday@yahoo.com>
2025-05-23 09:15:54 -0400

    25 01 24, 스프링부트 기초, 12강, 고객(브라우저)는 String 타입의 값만 이해한다, /home/plus?a=1&b=2, 쿼리스트링, URL 파라미터 @RequestParam(defaultValue = "0") int a

--- src/main/java/com/sbs/tutorial1/controller/HomeController.java
+++ src/main/java/com/sbs/tutorial1/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.sbs.tutorial1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,7 +13,7 @@ public class HomeController
 {
     @GetMapping("/home/main")
     //개발자가 /home/main 이르는 요청을 보내면  아래 메소드를 실행하라.
-    //http://localhost:8080/home/main
+    //localhost:8080/home/main
     @ResponseBody
     //아래 메소드를 실행해서 body에 출력해라.
     public String showHome()
@@ -21,7 +22,7 @@ public class HomeController
     }
 
     @GetMapping("/home/main2")
-    //http://localhost:8080/home/main2
+    //localhost:8080/home/main2
     @ResponseBody
     public String showHome2()
     {
@@ -29,10 +30,27 @@ public class HomeController
     }
 
     @GetMapping("/home/main3")
-    //http://localhost:8080/home/main3
+    //localhost:8080/home/main3
     @ResponseBody
     public String showHome3()
     {
         return "스프링부트 획기적이다.";
+    }
+
+    @GetMapping("/home/plus")
+    //localhost:8080/home/plus?a=1&b=1 --> 2
+    //try localhost:8080/home/plus?a=1&bbb=1 --> Error
+    //URL 요청시 넘겨준 파라미터 값이 매개변수에 들어간다.
+    @ResponseBody
+    //public int showPlus(int a, int b) //URL parameter 이름과 매개변수 이름이 일치해야 한다. (스프링부트 규칙)
+    //try localhost:8080/home/plus?a=1 --> Error
+    //public int showPlus(int a, @RequestParam int b) //localhost:8080/home/plus?a=1 --> Error
+    //@RequestParam 생략 가능
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b)
+    //localhost:8080/home/plus?a=1 --> good
+    //localhost:8080/home/plus?aaa=1 --> 0 --> bad
+    //localhost:8080/home/plus?aaa=1&bbb=1 --> 0 --> bad
+    {
+        return a + b;
     }
 }
