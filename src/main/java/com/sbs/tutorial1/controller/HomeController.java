7d7372250fb95f01e0e598f877d084a1d0fb3acb
thankallday <thankallday@yahoo.com>
2025-05-23 08:34:14 -0400

    24 25 01 24, 스프링부트 기초, 8강, /home/main 요청에 대한 응답처리, @Controller, @GetMapping, @ResponseBody

---
+++ src/main/java/com/sbs/tutorial1/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.sbs.tutorial1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+//개발자가 스프링부트에게
+//이 클래스는 웹 요청을 받아서 작업을 한다
+//이 클래스는 Controller 야!!!
+public class HomeController
+{
+    @GetMapping("/home/main")
+    //개발자가 /home/main 이르는 요청을 보내면  아래 메소드를 실행하라.
+    //http://localhost:8080/home/main
+    @ResponseBody
+    //아래 메소드를 실행해서 body에 출력해라.
+    public String showHome()
+    {
+        return "어서오세요";
+    }
+}
